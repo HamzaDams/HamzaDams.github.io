0ff71db6c6cc8b553ec930e756b1226d31c7434e
Hamza Dams <hddesignx@gmail.com>
2020-12-07 00:31:28 +0100

    add project

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink} from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <ul>
+                <NavLink to="/" exact className="hover" activeClassName="nav-active" />
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
