37a537c0af7a7c69967a56ab25b7fbff3448d71e
Hamza Dams <hddesignx@gmail.com>
2020-12-07 11:38:51 +0100

    Add navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,33 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <ul>
-                <NavLink to="/" exact className="hover" activeClassName="nav-active" />
+                <NavLink to="/" exact className="hover" activeClassName="nav-active">
+                    <li>Accueil</li>
+                </NavLink>
+
+                <li className="nav-portfolio">Portfolio
+                    <ul className="nav-projects">
+                        <NavLink to="/project-1" activeClassName="nav-active"
+                                 className="hover">
+                            <li>Project 1</li>
+                        </NavLink>
+                        <NavLink to="/project-2" activeClassName="nav-active"
+                                 className="hover">
+                            <li>Project 2</li>
+                        </NavLink>
+                        <NavLink to="/project-3" activeClassName="nav-active"
+                                 className="hover">
+                            <li>Project 3</li>
+                        </NavLink>
+                        <NavLink to="/project-4" activeClassName="nav-active"
+                                 className="hover">
+                            <li>Project 4</li>
+                        </NavLink>
+                    </ul>
+                </li>
+                <NavLink to="/contact" exact className="hover" activeClassName="nav-active">
+                    <li>Contact</li>
+                </NavLink>
             </ul>
         </div>
     );
